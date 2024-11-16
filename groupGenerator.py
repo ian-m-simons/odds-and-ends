1fceb7798ba47dd8a80d8c3be78d6cf1fcdb39f6
Ian Simons <iansimons93@gmail.com>
2024-11-15 20:21:54 -0500

    group assignment logic

--- groupGenerator.py
+++ groupGenerator.py
@@ -11,18 +11,21 @@ def inputInt(prompt):
             print("[ERROR] Input must be an integer")
     return groupCount
     
-def buildGroups(students, groupCount):
+def buildGroupsByGroupCount(students, groupCount):
     groupSize = len(students)//groupCount
     results = []
-    while (len(students)) > 0:
-        counter = 0
+    for i in range(groupCount):
         results.append([])
-        for i in range(groupSize):
-            randomNumber = random.randint(0,(len(students)-1))
-            results[counter].append(students[randomNumber])
-            students.pop(randomNumber)
-
-        counter += 1
+    groupNumber = 0
+    while (len(students)) > 0:
+        randomNumber = random.randint(0,(len(students)-1))
+        results[groupNumber].append(students[randomNumber])
+        students.pop(randomNumber)
+        if groupNumber == groupCount-1:
+            groupNumber = 0
+        else:
+            groupNumber += 1
+        
     return results
 
 
@@ -30,7 +33,7 @@ def main():
     rawStudentNames = input("please enter all student names, separate names with a comma\n")
     students = rawStudentNames.split(",")
     groupCount = inputInt("please enter the desired number of groups, students will be evenly divided among the given groups ")
-    results = buildGroups(students, groupCount)
+    results = buildGroupsByGroupCount(students, groupCount)
     print("groups: ")
     for i in range (len(results)):
         print("group ", (i+1), "\n", results[i],"\n")
