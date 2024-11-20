07020af9a82307f70efd0a6b97311b96ff0ef3f8
Ian Simons <iansimons93@gmail.com>
2024-11-20 16:34:14 -0500

    adding option 4 and fixing issues in option 2

--- randomPicker.py
+++ randomPicker.py
@@ -19,27 +19,33 @@ def selectStudent(students):
 
 def main():
     print("Welcome!")
+    raw_list = input("Please enter student names separated by a comma, press enter when all students have been added\n")
+    students = raw_list.split(",")
     while (True):
         print("Please select an option below!")
         print("1) Enter new list of students")
         print("2) Select Student at random")
         print("3) Reset existing list of students")
+        print("4) View remaining students")
         print("0) Exit Program")
         choice = inputInt("option: ")
         if choice == 1:
             raw_list = input("please enter student names separated by a comma, press enter when all students have been added\n")
             students = raw_list.split(",")
         elif choice == 2:
-            if len(students > 0):
+            if len(students) > 0:
                 students = selectStudent(students)
             else:
                 print("current list of students is empty, reset list or enter a new list")
         elif choice == 3:
             students = raw_list.split(",")
+        elif choice == 4:
+            for i in students:
+                print(i)
         elif choice == 0:
             exit(0)
         else:
-        print(students)
+            print("[ERROR] invalid selection")
 
 if __name__ == "__main__":
     main()
