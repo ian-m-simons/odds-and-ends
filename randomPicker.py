8437fc1eb40bcdca9b23d0e3018009d69e5ab9bd
Ian Simons <iansimons93@gmail.com>
2024-11-20 16:06:18 -0500

    first commit

---
+++ randomPicker.py
@@ -0,0 +1,24 @@
+import random
+
+def inputInt(prompt):
+    success = False
+    while (not success):
+        option = input(prompt)
+        try:
+            option = int(groupCount)
+            success = True
+        except:
+            print("[ERROR] Input must be an integer")
+    return option
+
+def main():
+    
+    while (True):
+        print("Welcome! please select an option below!")
+        print("1) Enter new list of students")
+        print("2) Select Student at random")
+        print("3) Reset existing list of students")
+        choice = inputInt("option: ")
+        if choice == 1:
+
+
