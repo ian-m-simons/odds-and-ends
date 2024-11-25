b8b14bd01815297b2a6bfb679abbf14b45d22a23
Ian Simons <iansimons93@gmail.com>
2024-11-25 13:39:38 -0500

    adding menu

--- coinFlipper.py
+++ coinFlipper.py
@@ -1,15 +1,33 @@
 import random
 
+def inputInt(prompt):
+    success = False
+    while (not success):
+        num = input(prompt)
+        try:
+            num = int(num)
+            success = True
+        except:
+            print("[ERROR] Input must be an integer")
+    return num
+
 def main():
-    choice = input("would you like to flip a coint? (yes/no): ")
-    if "y" in choice or "Y" in choice:
-        number = random.randint(0,1)
-        if number == 0:
-            print("HEADS!")
+    print("Welcom!")
+    while True:
+        print("please select an option")
+        print("1) flip coin")
+        print("0) exit")
+        choice = inputInt("option: ")
+        if choice == 1:
+            number = random.randint(0,1)
+            if number == 0:
+                print("HEADS!")
+            else:
+                print("Tails!")
+        elif choice == 0:
+            exit(0)
         else:
-            print("Tails!")
-    else:
-        exit(0)
+            print("[ERROR] invalid option" )
 
 if __name__ == "__main__":
     main()
