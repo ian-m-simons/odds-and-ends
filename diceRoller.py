46a64c14af1d147cb45607bfda32900e5f3a3840
ian-m-simons <iansimons93@gmail.com>
2024-12-01 12:28:09 -0500

    cleaning

--- diceRoller.py
+++ diceRoller.py
@@ -23,7 +23,7 @@ def main():
    sides = 6
    diceCount = 2
    while True:
-       print("what would you like to do?")
+       print("\nwhat would you like to do?")
        print("1) change type of dice (default 6 sided di)")
        print("2) change number of dice (default 2)")
        print("3) roll dice")
@@ -35,22 +35,9 @@ def main():
            diceCount = inputInt("how many dice would you like to have? ")
        elif choice == 3:
            rollDice(sides, diceCount)
-           print("\n\n\n")
        elif choice == 0:
            exit(0)
        else:
            print("[ERROR] invalid option")
-   '''
-    print("welcome!")
-    sides = inputInt("how many sides should your dice have? ")
-    print("ok we'll create ", sides, "sided dice!")
-    diceCount = inputInt("how many dice should we roll? ")
-    total = 0
-    for i in range(diceCount):
-        landing = random.randint(1, sides)
-        print("di ", i+1, "landed on ", landing)
-        total += landing
-    print("total comes to ", total)
-'''
 if __name__ == "__main__":
     main()
