b92044deba13f41a4fad4c6c273815901da5bc30
Ian Simons <iansimons93@gmail.com>
2024-12-04 09:12:55 -0500

    erase previous line and add input validation

--- timer.py
+++ timer.py
@@ -1,9 +1,24 @@
 import time
 
-timer = int(input("Please enter time "))
+def inputInt(prompt):
+    success = False
+    while (not success):
+        num = input(prompt)
+        try:
+            num = int(num)
+            success = True
+        except:
+            print("[ERROR] Input must be an integer")
+    return num
 
-while timer > 0:
-    print(timer, "\r")
-    timer -= 1
-    time.sleep(1)
-print("Timer has expired")
+def main():
+    timer = inputInt("Please enter time in seconds ")
+
+    while timer > 0:
+        print(timer, end='\r')
+        timer -= 1
+        time.sleep(1)
+    print("Timer has expired")
+
+if __name__ == "__main__":
+    main()
