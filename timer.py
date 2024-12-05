c63510a892af92a1bf477202037c9e1f58fe3b01
Ian Simons <iansimons93@gmail.com>
2024-12-05 11:48:17 -0500

    beginning work on todo

--- timer.py
+++ timer.py
@@ -1,4 +1,4 @@
-import time
+from datetime import datetime
 
 def inputInt(prompt):
     success = False
@@ -15,10 +15,19 @@ def main():
     timer = inputInt("Please enter time in seconds ")
 #todo change timer so it uses the seconds from date time rather than sleep
 #this will make it more accurate
+    dt = datetime.now()
+    dt = dt.microsecond
     while timer > 0:
-        print(timer, end='\r')
-        timer -= 1
-        time.sleep(1)
+        ct = datetime.now()
+        ct = ct.microsecond
+        if int(ct) >= int(dt) + 1000000:
+            print(timer, end='\r')
+            timer -= 1
+            dt = datetime.now()
+            dt = dt.microsecond
+        else:
+            pass
+        
     print("Timer has expired")
 
 if __name__ == "__main__":
