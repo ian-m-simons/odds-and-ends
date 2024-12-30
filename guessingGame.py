3049a7d22f89a4ec32fce2184bf9d9f75e3dac84
Ian Simons <iansimons93@gmail.com>
2024-12-30 09:22:54 -0500

    touch ups

--- guessingGame.py
+++ guessingGame.py
@@ -16,15 +16,16 @@ def main():
     print("I have picked a number between 1 and 100, can you guess what it is?")
     num = random.randint(1,100)
     guess = inputInt("what is your guess? ")
-    while guess != num:
-        guess = inputInt("What is your next guess? ")
+    while True:
         if guess > num:
             print("too big try again")
+            guess = inputInt("what is your next guess? ")
         elif guess < num:
             print("too small try again")
+            guess = inputInt("what is your next guess? ")
         else:
-            pass
-    print("That's correct! you've won!")
+            print("that's correct, you win!")
+            break
 
 if __name__ == "__main__":
     main()
