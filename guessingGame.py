c9719fb841d601670e76d4b8016f2f30716560f3
Ian Simons <iansimons93@gmail.com>
2024-12-30 09:18:58 -0500

    fixing errors

--- guessingGame.py
+++ guessingGame.py
@@ -1,30 +1,30 @@
 import random
 
 def inputInt(prompt):
-	success = false
+	success = False
 	while not success:
 		try:
 			var = input(prompt)
 			var = int(var)
-			success = true
+			success = True
 		except:
 			print("[ERROR] input must be an integer")
 	return var
 
 def main():
-	print("Lets play a guessing game!")
+    print("Lets play a guessing game!")
     print("I have picked a number between 1 and 100, can you guess what it is?")
-    num = random.randInt(1,100)
+    num = random.randint(1,100)
     guess = inputInt("what is your guess? ")
     while guess != num:
+        guess = inputInt("What is your next guess? ")
         if guess > num:
             print("too big try again")
-            guess = inputInt("what is your next guess? ")
         elif guess < num:
             print("too small try again")
-            guess = inputInt("what is your next guess? ")
         else:
-            print("that's correct!\nYou've won!")
+            pass
+    print("That's correct! you've won!")
 
 if __name__ == "__main__":
     main()
